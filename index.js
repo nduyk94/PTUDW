7985ee40366abab0002c0b47a0f798a77d26411f
Khanh <nduyk94@gmail.com>
2021-02-18 15:23:07 +0700

    commit port 80

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/:page', (req, res) => {
 
 
 // Set Server Port & Start Server
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 80);
 app.listen(app.get('port'), () => {
     console.log(`Server is running at port ${app.get('port')}`);
 });
